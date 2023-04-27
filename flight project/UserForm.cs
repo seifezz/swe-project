a17546862315ba12a179afee5a4030e9011281f0
seif amr <seifamr410@gmail.com>
2023-04-28 01:14:52 +0300

    fix

--- flight project/UserForm.cs
+++ flight project/UserForm.cs
@@ -44,5 +44,21 @@ namespace flight_project
             cancel.Show();
             this.Hide();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            signinform form = new signinform();
+            Program.globaleEmail = "";
+            form.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Searchform sf = new Searchform();
+            sf.Show();
+            this.Hide();
+
+        }
     }
 }

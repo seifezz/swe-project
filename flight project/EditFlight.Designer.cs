3b7f6b089a8b00f2308467c860e87807c982a24d
Seif Omran <41484353+seifomran132@users.noreply.github.com>
2023-04-27 13:02:27 +0200

    Edit Flight in disconnected mode

---
+++ flight project/EditFlight.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace flight_project
+{
+    partial class EditFlight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_editflight = new System.Windows.Forms.Label();
+            this.btn_apply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(58, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 294);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txt_editflight
+            // 
+            this.txt_editflight.AutoSize = true;
+            this.txt_editflight.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_editflight.Location = new System.Drawing.Point(53, 19);
+            this.txt_editflight.Name = "txt_editflight";
+            this.txt_editflight.Size = new System.Drawing.Size(139, 29);
+            this.txt_editflight.TabIndex = 1;
+            this.txt_editflight.Text = "Edit Flights";
+            this.txt_editflight.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_apply
+            // 
+            this.btn_apply.Location = new System.Drawing.Point(609, 382);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(136, 23);
+            this.btn_apply.TabIndex = 2;
+            this.btn_apply.Text = "Apply Changes";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
+            // 
+            // EditFlight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_apply);
+            this.Controls.Add(this.txt_editflight);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "EditFlight";
+            this.Text = "EditFlight";
+            this.Load += new System.EventHandler(this.EditFlight_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label txt_editflight;
+        private System.Windows.Forms.Button btn_apply;
+    }
+}
---
+++ flight project/EditFlight.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace flight_project
+{
+    public partial class EditFlight : Form
+    {
+
+        OracleDataAdapter adapter;
+        OracleCommandBuilder builder;
+        DataSet ds;
+
+        public EditFlight()
+        {
+            InitializeComponent();
+        }
+
+        private void EditFlight_Load(object sender, EventArgs e)
+        {
+            string connstr = "Data Source=orcl; User Id=Scott; Password=tiger;";
+            string cmdstr = "select * from flightinfo";
+
+            adapter = new OracleDataAdapter(cmdstr, connstr);
+            ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            builder = new OracleCommandBuilder(adapter);
+            adapter.Update(ds.Tables[0]);
+            MessageBox.Show("Data Saved");
+        }
+    }
+}
